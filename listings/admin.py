32626547e32ccfc8a668e6bd1b344fdd4a97cf06
piyushjhawar <piyushjhawar24@gmail.com>
2019-09-27 17:06:33 -0700

    Made Listing Home and About Page Dynamic made changes to templates

--- listings/admin.py
+++ listings/admin.py
@@ -3,6 +3,17 @@ from .models import Listing
 
 # Register your models here.
 
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published','price', 'list_date', 'realtor')
+    list_display_links = ('id','title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
+    list_per_page = 25
 
-admin.site.register(Listing)
+
+
+admin.site.register(Listing,ListingAdmin)
+
+
 
--- realtors/admin.py
+++ realtors/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import Realtor
 
 # Register your models here.
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'phone', 'hire_date')
+    list_display_links = ('id','name')
+    list_editable = ('phone',)
+    search_fields = ('name',)
+    list_per_page = 25
 
-admin.site.register(Realtor)
+
+admin.site.register(Realtor, RealtorAdmin)
