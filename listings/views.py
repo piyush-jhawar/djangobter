8d1ba3cfe5cadc378d6f8324603a0684094972b5
piyushjhawar <piyushjhawar24@gmail.com>
2019-09-27 19:06:15 -0700

    Completed the setup for Search Page

--- listings/views.py
+++ listings/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing
 from realtors.models import Realtor
+from .choices import bedroom_choices, price_choices, state_choices
 
 # Create your views here.
 
@@ -26,4 +27,10 @@ def listing(request, listing_id):
     return render(request,'listings/listing.html', context)
 
 def search(request):
-    return render(request,'listings/search.html')
+
+    context = {
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'state_choices': state_choices
+    }
+    return render(request,'listings/search.html', context)
